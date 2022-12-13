8285b617f9e71d84a3d43cce02db9fb34555f562
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-13 13:10:09 -0500

    some JS and CSS added

--- README.md
+++ README.md
@@ -2,25 +2,13 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+A short quiz to test the user's skills on HTML,CSS,Java
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+No installation required
 
 ## Usage
 
--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-//start button
-//questions
-//answers
-//save score
-//ask initials 
-//save score with initials
-//time remaining countdown 
+//Globaly defind variables
+let startQuiz = document.querySelector("start")
+let startTime = document.querySelector("timer")
+
+
+// event listent - start quiz button - start quiz questions
+        // roll through 1 by 1 - driven by user's answer "click"
+startQuiz.addEventListener("click", function(0) {
+        //code to roll questions
+        //Questions ( no min question required)
+                //HTML
+                //CSS
+                //JAVA
+        //multiple choice answers 
+        //maybe an alert for prior question correct/incorrect?
+
+});
+
+
+// event listener - start quiz button - start timer 
+startQuiz.addEventListener("click", function(){
+        //count down starting at 60 seconds
+        ////Quiz ends at 0 time or no more questions
+
+}),
+
+
+
+//User input box - initials & Score saves to local
